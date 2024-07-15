3a89b3b99687653b2006b8be2635b31ff322ef42
YuXiangLin <evan900527@gmail.com>
2024-07-16 06:36:29 +0800

    transfer data

--- SoundCodec/dataset/__init__.py
+++ SoundCodec/dataset/__init__.py
@@ -1,3 +1,6 @@
+import sys  
+sys.path.append("workspace/Codec-SUPERB/SoundCodec")
+
 def load_dataset(dataset_name):
     module = __import__(f"dataset.{dataset_name}", fromlist=[dataset_name])
     return module.load_data()
